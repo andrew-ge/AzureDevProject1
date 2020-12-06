f82df3e552b76e3c2e4eca50a78d90a157237323
andrew-ge <59096310+andrew-ge@users.noreply.github.com>
2020-12-05 23:43:35 -0600

    fixed error
    some strang if ident

--- FlaskWebProject/views.py
+++ FlaskWebProject/views.py
@@ -22,8 +22,7 @@ imageSourceUrl = 'https://'+ app.config['BLOB_ACCOUNT']  + '.blob.core.windows.n
 def home():
     user = User.query.filter_by(username=current_user.username).first_or_404()
     posts = Post.query.all()
-	
-	return render_template(
+    return render_template(
         'index.html',
         title='Home Page',
         posts=posts
@@ -88,7 +87,7 @@ def authorized():
     if request.args.get('code'):
         cache = _load_cache()
         # TODO: Acquire a token from a built msal app, along with the appropriate redirect URI
-        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code( 
+        result = _build_msal_app(cache=cache).acquire_token_by_authorization_code(
 			request.args['code'],
 			scopes=Config.SCOPE,
 			redirect_uri=url_for('authorized', _external=True, _scheme='https'))
@@ -124,18 +123,20 @@ def _load_cache():
 
 def _save_cache(cache):
     # TODO: Save the cache, if it has changed
-	if cache.has_state_changed:
-        session["token_cache"] = cache.serialize()
+    if cache.has_state_changed:
+          session["token_cache"] = cache.serialize()
+
 
 def _build_msal_app(cache=None, authority=None):
     # TODO: Return a ConfidentialClientApplication
-    return  return msal.ConfidentialClientApplication(
-     Config.CLIENT_ID, authority=authority or Config.AUTHORITY,
-     client_credential=Config.CLIENT_SECRET, token_cache=cache)
+    return  msal.ConfidentialClientApplication(
+    Config.CLIENT_ID,
+    authority=authority or Config.AUTHORITY,
+    client_credential=Config.CLIENT_SECRET, token_cache=cache)
 
 def _build_auth_url(authority=None, scopes=None, state=None):
     # TODO: Return the full Auth Request URL with appropriate Redirect URI
     return _build_msal_app(authority=authority).get_authorization_request_url(
 		scopes or [],
-		state=state or str(uuid.uuid4()),		
+		state=state or str(uuid.uuid4()),
 		redirect_uri=url_for('authorized', _external=True, _scheme='https'))
